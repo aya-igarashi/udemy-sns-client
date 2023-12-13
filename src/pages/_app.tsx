28de0b82e68edaf1171b38f8be303226a060574b
igarashi.aya <igarashi.aya@skyseer.co.jp>
2023-12-13 18:03:03 +0900

    done

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,18 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
+import Navbar from '@/components/Navbar'
+import { AuthProvider } from '@/context/auth'
 
+// ここにNavbarを入れると全てのページに反映される
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      {/* Authproviderでラップすることにより間に挟まれた部分がchirdrenとして認識され
+      どのコンポーネントでもvalueがglobalで使えるようになる */}
+      <AuthProvider>
+        <Navbar />
+        <Component {...pageProps} />
+      </AuthProvider>
+    </div>
+  )
 }
