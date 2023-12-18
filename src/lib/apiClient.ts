9d83c97906266ff0595472dace1daefd84f52b24
igarashi.aya <igarashi.aya@skyseer.co.jp>
2023-12-18 23:38:08 +0900

    baseURL

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "http://localhost:5080/api",
+  // baseURL: "http://localhost:5080/api",
+  baseURL: process.env.EXT_PUBLIC_API_BASEURL,
   headers: {
     "Content-Type": "application/json",
   },
