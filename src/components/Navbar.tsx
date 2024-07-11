d5d0ac2f2b36cee9397d9645c3d05d0ca9e43264
igarashi.aya <igarashi.aya@skyseer.co.jp>
2024-07-11 14:44:13 +0900

    login

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,8 @@
-// rafceタブ補完
 import { useAuth } from '@/context/auth';
-import Link from 'next/link'
-import React from 'react'
+import Link from 'next/link';
+import React from 'react';
 
 const Navbar = () => {
-
   const { user, logout } = useAuth();
 
   return (
@@ -12,7 +10,8 @@ const Navbar = () => {
       <div className="container mx-auto flex justify-between items-center">
         <h1 className="font-semibold text-xl">
           <Link href="/" className="text-2xl font-medium">
-            SNS Clone</Link>
+            SNS Clone
+          </Link>
         </h1>
         <nav>
           <ul className="flex space-x-4">
@@ -34,7 +33,7 @@ const Navbar = () => {
             ) : (
               <>
                 <Link
-                  href={"/login"}
+                  href="/login"
                   className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
                 >
                   ログイン
@@ -51,7 +50,66 @@ const Navbar = () => {
         </nav>
       </div>
     </header>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
+
+
+// // rafceタブ補完
+// import { useAuth } from '@/context/auth';
+// import Link from 'next/link'
+// import React from 'react'
+
+// const Navbar = () => {
+
+//   const { user, logout } = useAuth();
+
+//   return (
+//     <header className="bg-gray-700 p-4 text-white">
+//       <div className="container mx-auto flex justify-between items-center">
+//         <h1 className="font-semibold text-xl">
+//           <Link href="/" className="text-2xl font-medium">
+//             SNS Clone</Link>
+//         </h1>
+//         <nav>
+//           <ul className="flex space-x-4">
+//             {user ? (
+//               <>
+//                 <Link
+//                   href={`/profile/${user.id}`}
+//                   className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+//                 >
+//                   プロフィール
+//                 </Link>
+//                 <button
+//                   onClick={logout}
+//                   className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+//                 >
+//                   ログアウト
+//                 </button>
+//               </>
+//             ) : (
+//               <>
+//                 <Link
+//                   href={"/login"}
+//                   className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+//                 >
+//                   ログイン
+//                 </Link>
+//                 <Link
+//                   href="/signup"
+//                   className="bg-white text-gray-900 py-2 px-3 rounded-lg font-medium"
+//                 >
+//                   サインアップ
+//                 </Link>
+//               </>
+//             )}
+//           </ul>
+//         </nav>
+//       </div>
+//     </header>
+//   )
+// }
+
+// export default Navbar
--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -17,7 +17,6 @@ interface AuthProviderProps {
 };
 
 const AuthContext = React.createContext<AuthContextType>({
-  // 初期値
   user: null,
   login: () => { },
   logout: () => { }
@@ -37,29 +36,27 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
 
   useEffect(() => {
     const token = localStorage.getItem("auth_token");
-    if (!token) {
+    if (token) {
+      apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
+
+      apiClient.get("/users/find").then((res) => {
+        setUser(res.data.user);
+      }).catch((error) => {
+        console.log(error);
+        router.push("/login");
+      });
+    } else {
       router.push("/login");
     }
-    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`
-
-    apiClient.get("/users/find").then((res) => {
-      setUser(res.data.user);
-    })
-      .catch((error) => {
-        console.log(error);
-      });
-
-  }, [])
+  }, []);
 
   const login = async (token: string) => {
     localStorage.setItem("auth_token", token);
-    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`
+    apiClient.defaults.headers["Authorization"] = `Bearer ${token}`;
 
     try {
-      await apiClient.get("/users/find").then((res) => {
-        setUser(res.data.user);
-      });
-
+      const res = await apiClient.get("/users/find");
+      setUser(res.data.user);
     } catch (error) {
       console.log(error);
     }
@@ -69,13 +66,98 @@ export const AuthProvider = ({ children }: AuthProviderProps) => {
     localStorage.removeItem("auth_token");
     delete apiClient.defaults.headers["Authorization"];
     setUser(null);
+    router.push("/login");
   };
 
   const value = {
     user,
     login,
     logout,
-  }
+  };
 
-  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
 };
+
+
+
+// import apiClient from "@/lib/apiClient";
+// import { useRouter } from "next/router";
+// import React, { ReactNode, useContext, useEffect, useState } from "react";
+
+// interface AuthContextType {
+//   user: null | {
+//     id: number;
+//     username: string;
+//     email: string;
+//   };
+//   login: (token: string) => void;
+//   logout: () => void;
+// };
+
+// interface AuthProviderProps {
+//   children: ReactNode;
+// };
+
+// const AuthContext = React.createContext<AuthContextType>({
+//   // 初期値
+//   user: null,
+//   login: () => { },
+//   logout: () => { }
+// });
+
+// export const useAuth = () => {
+//   return useContext(AuthContext);
+// };
+
+// export const AuthProvider = ({ children }: AuthProviderProps) => {
+//   const router = useRouter();
+//   const [user, setUser] = useState<null | {
+//     id: number;
+//     username: string;
+//     email: string;
+//   }>(null);
+
+//   useEffect(() => {
+//     const token = localStorage.getItem("auth_token");
+//     if (!token) {
+//       router.push("/login");
+//     }
+//     apiClient.defaults.headers["Authorization"] = `Bearer ${token}`
+
+//     apiClient.get("/users/find").then((res) => {
+//       setUser(res.data.user);
+//     })
+//       .catch((error) => {
+//         console.log(error);
+//       });
+
+//   }, [])
+
+//   const login = async (token: string) => {
+//     localStorage.setItem("auth_token", token);
+//     apiClient.defaults.headers["Authorization"] = `Bearer ${token}`
+
+//     try {
+//       await apiClient.get("/users/find").then((res) => {
+//         setUser(res.data.user);
+//       });
+
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   const logout = () => {
+//     localStorage.removeItem("auth_token");
+//     delete apiClient.defaults.headers["Authorization"];
+//     setUser(null);
+//   };
+
+//   const value = {
+//     user,
+//     login,
+//     logout,
+//   }
+
+//   return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+// };
